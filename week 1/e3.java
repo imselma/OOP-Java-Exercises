c59b528b50a088f1eb90c62a3409cace2c4c155f
Selma Imširović <116001190+imselma@users.noreply.github.com>
2023-05-17 12:00:22 +0200

    Add files via upload

---
+++ week 1/e3.java
@@ -0,0 +1,8 @@
+public class e3 {
+    public static void main(String[] args) {
+
+        int numberOfSeconds = 60*60*24*365;
+        System.out.println("There are " + numberOfSeconds +  " seconds in year.");
+        
+    }
+}
